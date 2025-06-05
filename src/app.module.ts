35f5241c6d29db1699ffae971ca97047f5a194dc
Yury Sidyakin <yurafikys@gmail.com>
2025-06-05 21:26:32 +0300

    feat: Connect MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://localhost/test'),
+    // MongooseModule.forRoot('mongodb://localhost/test'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
